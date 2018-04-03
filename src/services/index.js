0a4b51f9f8a1bebaf0fcdab03a2a622dbfaf226e
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-04-02 22:02:25 -0400

    Create a default authentication service with Google, Github, local auth

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const application = require('./application/application.service.js');
+const users = require('./users/users.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(application);
+  app.configure(users);
 };
