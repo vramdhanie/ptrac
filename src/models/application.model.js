f15bf4126bc647549c8be25547fa3395d3b8e5fd
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-04-03 09:55:34 -0400

    Modify user object and add authentication to application

--- src/models/application.model.js
+++ src/models/application.model.js
@@ -1,12 +1,18 @@
 // application-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const application = new Schema({
-    text: { type: String, required: true }
+    name: {type: String, required: true},
+    username: {type: String},
+    url: {type: String},
+    hint: {type: String},
+    pin: {type: String},
+    text: { type: String, required: true },
+    archive: {type: Boolean, default: false}
   }, {
     timestamps: true
   });
--- src/models/users.model.js
+++ src/models/users.model.js
@@ -5,15 +5,17 @@
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const users = new mongooseClient.Schema({
-  
+
     email: {type: String, unique: true},
     password: { type: String },
-  
-  
+
+
     googleId: { type: String },
-  
+
     githubId: { type: String },
-  
+    firstName: {type: String},
+    lastName: {type: String}
+
   }, {
     timestamps: true
   });
