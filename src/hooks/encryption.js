96a9cc3f18dea8e7757b5c79c3b9974579324571
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-04-03 20:04:27 -0400

    Add encryption hook

---
+++ src/hooks/encryption.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const crypto = require('crypto');
+
+// eslint-disable-next-line no-unused-vars
+module.exports = function (options = {}) {
+  return async context => {
+
+    return context;
+  };
+};
